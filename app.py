7db40ce535f4096fc5928d9aed3c88c29e417885
arvin <arvinsaguisa@gmail.com>
2024-09-09 00:25:31 +0800

    added templates

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(_name_, static_folder='assets', static_url_path='/static')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+@app.route('/evaluation')
+def evaluation():
+    return render_template('evaluation.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
